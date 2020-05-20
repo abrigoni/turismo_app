15516777dfa613eac33ead37c97a914f26e9235e
brigoni <brian.rigoni1@gmail.com>
2020-05-20 20:10:27 -0300

    alojamientos filtro con get de datos requeridos

--- lib/pages/filtros_alojamientos_page.dart
+++ lib/pages/filtros_alojamientos_page.dart
@@ -1,9 +1,46 @@
+import 'package:app/models/categoria_model.dart';
+import 'package:app/models/clasificacion_model.dart';
 import 'package:flutter/material.dart';
+import 'package:app/providers/categoria_provider.dart';
+import 'package:app/providers/clasificacion_provider.dart';
 
 
-class FiltrosAlojamientosPage extends StatelessWidget {
+class FiltrosAlojamientosPage extends StatefulWidget {
 
   static const String ROUTENAME = 'FiltroAlojamientosPage';
+  final ClasificacionProvider clasificacionProvider;
+  final CategoriaProvider categoriaProvider;
+
+  FiltrosAlojamientosPage({
+    @required this.clasificacionProvider,
+    @required this.categoriaProvider,
+  });
+
+  @override
+  _FiltrosAlojamientosPageState createState() => _FiltrosAlojamientosPageState();
+}
+
+class _FiltrosAlojamientosPageState extends State<FiltrosAlojamientosPage> {
+
+  List<Categoria> categorias;
+  List<Clasificacion> clasificaciones;
+
+  @override 
+  void initState() {
+    super.initState();
+    getClasificaciones();
+    getCategorias();
+  }
+
+  void getClasificaciones() async {
+    clasificaciones = await widget.clasificacionProvider.getClasificaciones();
+    print(clasificaciones[0].nombre);
+  }
+
+  void getCategorias() async {
+    categorias = await widget.categoriaProvider.getCategorias();
+    print(categorias[0].estrellas);
+  }
 
   @override
   Widget build(BuildContext context) {
