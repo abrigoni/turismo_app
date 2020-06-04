a8a4b734508bc00b0dd562df1b8c280dfceaa11a
brigoni <brian.rigoni1@gmail.com>
2020-06-04 13:01:27 -0300

    bloc abstract class y bloc de alojamiento

---
+++ lib/BLoC/alojamientos_bloc.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:app/BLoC/bloc.dart';
+import 'package:app/models/alojamiento_model.dart';
+import 'package:app/providers/alojamiento_provider.dart';
+
+class AlojamientosBloc implements Bloc {
+
+  static final AlojamientosBloc _singleton = new AlojamientosBloc._internal();
+
+  factory AlojamientosBloc() {
+    return _singleton;
+  }
+
+
+  AlojamientosBloc._internal() {
+    obtenerAlojamientos();
+  }
+
+  final _alojamientosController = StreamController<List<Alojamiento>>.broadcast();
+
+  Stream<List<Alojamiento>> get alojamientosStream => _alojamientosController.stream;
+
+  @override
+  void dispose() {
+    _alojamientosController?.close();
+  }
+
+  obtenerAlojamientos() async {
+    _alojamientosController.sink.add( await AlojamientoProvider().getAlojamientos());
+  }
+
+  filtrarAlojamientos() async {
+    // @ TODO
+  }
+
+}
