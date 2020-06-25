a4b9aaf008626d0efe3bc857cf529ff70580260b
brigoni <brian.rigoni1@gmail.com>
2020-06-25 06:55:22 -0300

    refactor a main

--- lib/BLoC/bloc.dart
+++ lib/BLoC/bloc.dart
@@ -1,3 +1,4 @@
+export './simple_bloc_delegate.dart';
 export './alojamiento_bloc.dart';
 export './alojamiento_event.dart';
 export './alojamiento_state.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,31 @@
-import 'package:app/BLoC/alojamiento_event.dart';
-import 'package:app/data/repositories/alojamiento_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
-import 'package:app/routes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:app/BLoC/simple_bloc_delegate.dart';
+import 'package:app/routes.dart';
+import 'package:app/BLoC/alojamiento_event.dart';
 import 'package:app/BLoC/alojamiento_bloc.dart';
-
+import 'package:app/data/repositories/alojamiento_repository.dart';
 import 'package:app/presentation/screens/home_screen.dart';
-
-
-class SimpleBlocDelegate extends BlocDelegate {
-  @override
-  void onEvent(Bloc bloc, Object event) {
-    print(event);
-    super.onEvent(bloc, event);
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    print(transition);
-    super.onTransition(bloc, transition);
-  }
-
-  @override
-  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
-    print(error);
-    super.onError(bloc, error, stackTrace);
-  }
-}
 
 void main() {
   BlocSupervisor.delegate = SimpleBlocDelegate();
-  final alojamientoRepository = AlojamientoRepository(); 
-
-  runApp( 
-    BlocProvider(
-      create: (context) {
-        return AlojamientosBloc(
-          alojamientoRepository: alojamientoRepository
-        )..add(AlojamientosLoaded());
-      },
-      child: MyApp(alojamientoRepository: alojamientoRepository)
-    )
-  );
+  runApp(MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
-
-  final AlojamientoRepository alojamientoRepository;
-
-  MyApp({Key key, @required this.alojamientoRepository}) : super(key:key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return BlocProvider<AlojamientosBloc>(
+      create: (context) => AlojamientosBloc( alojamientoRepository: AlojamientoRepository())..add( AlojamientosLoaded() ) ,
+      child: MaterialApp(
         title: 'Turismo App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-          body: BlocProvider<AlojamientosBloc>(
-            create: (context) => AlojamientosBloc(alojamientoRepository: alojamientoRepository),
-            child: HomeScreen(),
-          )
-        ),
-        routes: routes
-      );
+        initialRoute: HomeScreen.ROUTENAME,
+        routes: routes),
+    );
   }
 }
