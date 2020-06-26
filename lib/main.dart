b078d91e56903b0b78c7ef749835b1015f934235
brigoni <brian.rigoni1@gmail.com>
2020-06-26 07:31:01 -0300

    main con multiblocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:app/BLoC/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:app/BLoC/simple_bloc_delegate.dart';
 import 'package:app/routes.dart';
-import 'package:app/BLoC/alojamiento_event.dart';
-import 'package:app/BLoC/alojamiento_bloc.dart';
+import 'package:app/data/repositories/gastronomico_repository.dart';
 import 'package:app/data/repositories/alojamiento_repository.dart';
 import 'package:app/presentation/screens/home_screen.dart';
+import 'BLoC/bloc.dart';
 
 void main() {
   BlocSupervisor.delegate = SimpleBlocDelegate();
@@ -16,8 +17,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<AlojamientosBloc>(
-      create: (context) => AlojamientosBloc( alojamientoRepository: AlojamientoRepository())..add( AlojamientosLoaded() ) ,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<GastronomicosBloc>(
+          create: (context) => GastronomicosBloc( gastronomicoRepository: GastronomicoRepository())..add( GastronomicosFetch() )
+        ),
+        BlocProvider<AlojamientosBloc>( 
+          create: (context) => AlojamientosBloc( alojamientoRepository: AlojamientoRepository())..add( AlojamientosFetch() ) 
+        ),
+      ],
       child: MaterialApp(
         title: 'Turismo App',
         debugShowCheckedModeBanner: false,
