566b4593502a491303a8eaa3d608a8cb071f1860
brigoni <brian.rigoni1@gmail.com>
2020-07-13 18:22:42 -0300

    gesturedetector en lupa de search

--- lib/presentation/widgets/searchbar_widget.dart
+++ lib/presentation/widgets/searchbar_widget.dart
@@ -8,38 +8,30 @@ class SearchBarWidget extends StatefulWidget {
 
 class _SearchBarWidgetState extends State<SearchBarWidget> {
   
-  String _busqueda;
-  TextStyle textStyle;
-  final controller = TextEditingController();
-
-  _SearchBarWidgetState() {
-    this._busqueda = "";
-    this.textStyle = TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+  String _busqueda = "";
+  TextStyle textStyle = TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
       child: TextField( 
-        controller: controller,
         style: textStyle,
         decoration: InputDecoration(
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(14.0),
           ),
           hintText: "Buscar",
-          suffixIcon: Icon(Icons.search, color: Colors.white),
+          suffixIcon: GestureDetector(child: Icon(Icons.search, color: Colors.white), onTap: onSearchTap),
           hintStyle: textStyle
         ),
         onChanged: (busqueda){ _busqueda = busqueda; },
       ),
     );
   }
+
+  void onSearchTap() {
+    print(_busqueda);
+  }
+
 }
