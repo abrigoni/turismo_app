ab67f6ba52b618a67c6753accb3e9dd6ff1d5bb0
brian rigoni <brian.rigoni1@gmail.com>
2020-07-26 21:21:01 -0300

    borrado de blocdelegate

--- lib/BLoC/bloc.dart
+++ lib/BLoC/bloc.dart
@@ -1,4 +1,3 @@
-export './simple_bloc_delegate.dart';
 export 'alojamientos_bloc/alojamientos_bloc.dart';
 export 'gastronomicos_bloc/gastronomicos_bloc.dart';
 export 'favoritos_bloc/favoritos_bloc.dart';
